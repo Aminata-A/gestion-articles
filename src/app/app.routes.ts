5abd85c7cb362432dffc6670481610f053078178
Aminata-A <aminataassane.ndiaye@gmail.com>
2024-07-31 13:39:31 +0000

    mise en place de l'affichage des articles et mise en place des routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { ArticlesListComponent } from './articles-list/articles-list.component';
+import { ArticleFormComponent } from './article-form/article-form.component';
+import { ArticleDetailComponent } from './article-detail/article-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/articles', pathMatch: 'full' },
+  { path: 'articles', component: ArticlesListComponent },
+  { path: 'article/create', component: ArticleFormComponent },
+  { path: 'article/edit/:id', component: ArticleFormComponent },
+  { path: 'article/:id', component: ArticleDetailComponent }
+];
